735258c212e2acb011d9399a1075773e9d36dfe7
耗子 <haozi@loli.email>
2026-02-23 22:26:56 +0800

    feat: 调整排序

--- web/src/views/firewall/IndexView.vue
+++ web/src/views/firewall/IndexView.vue
@@ -11,24 +11,24 @@ import SettingView from '@/views/firewall/SettingView.vue'
 import { useGettext } from 'vue3-gettext'
 
 const { $gettext } = useGettext()
-const currentTab = ref('scan')
+const currentTab = ref('rule')
 </script>
 
 <template>
   <common-page show-header show-footer>
     <template #tabbar>
       <n-tabs v-model:value="currentTab" animated>
-        <n-tab name="scan" :tab="$gettext('Scan Awareness')" />
         <n-tab name="rule" :tab="$gettext('Port Rules')" />
         <n-tab name="ip-rule" :tab="$gettext('IP Rules')" />
         <n-tab name="forward" :tab="$gettext('Port Forwarding')" />
+        <n-tab name="scan" :tab="$gettext('Scan Awareness')" />
         <n-tab name="setting" :tab="$gettext('Settings')" />
       </n-tabs>
     </template>
-    <scan-view v-if="currentTab === 'scan'" />
     <rule-view v-if="currentTab === 'rule'" />
     <ip-rule-view v-if="currentTab === 'ip-rule'" />
     <forward-view v-if="currentTab === 'forward'" />
+    <scan-view v-if="currentTab === 'scan'" />
     <setting-view v-if="currentTab === 'setting'" />
   </common-page>
 </template>
